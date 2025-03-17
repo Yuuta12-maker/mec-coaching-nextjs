1e85f25036893944310f644dd5ea1b3e425afc28
Yuta Moriyama <anosubarasiiaiwomouitido_honor@yahoo.co.jp>
2025-03-17 13:14:39 +0900

    Tailwind CSS設定を追加

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#c50502',
+          dark: '#8e0000',
+          light: '#f8d7d7'
+        }
+      },
+      fontFamily: {
+        sans: ['Noto Sans JP', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
